0049a800b0e440d9fe90094d8258fc62ec9fc5f4
hakimassouane <hakim.assouane@epitech.eu>
2020-02-13 02:24:35 +0100

    Implemented basic breadcrumb feature

--- src/Components/GeneralComponents/Breadcrumb/BreadcrumbComponent.js
+++ src/Components/GeneralComponents/Breadcrumb/BreadcrumbComponent.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
-import { Breadcrumb } from 'react-bootstrap';
+import Breadcrumbs  from 'react-router-dynamic-breadcrumbs';
+
+const routes = {
+    '/': 'Home',
+    '/backgrounds': 'Backgrounds',
+    '/classes': 'Classes',
+    '/feats': 'Feats',
+    '/items': 'Items',
+    '/races': 'Races',
+    '/spells': 'Spells'
+}
 
 class BreadcrumbComponent extends Component {
     render () {
         return (
-            <Breadcrumb>
-            <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-            <Breadcrumb.Item active>Backgrounds</Breadcrumb.Item>
-            </Breadcrumb>
+            <Breadcrumbs mappedRoutes={routes} />
         );
     }
 }
