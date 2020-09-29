fb4cc0e2bcb86d8495d45de726f4047cce3683bd
fabrice dano <dano.fabrice@gmail.com>
2020-09-29 16:57:23 +0200

    reformate code

--- src/Services/user.js
+++ src/Services/user.js
@@ -3,27 +3,12 @@ import { Api } from '../Api/axios';
 const UserService = {
 
     async logUser(email, password) {
-        /*const valideEmailType = this.validEmailType(email);
-        if (!valideEmailType) {
-            throw new Error('Email is not valid');
-        }
-        const valideEmailAndPasswod = this.validEmailAndPassword(email, password);
-        if (valideEmailAndPasswod) {*/
-            return await Api.getInstance().post('/users/login', { email, password });
-       /* }
-        throw new Error('Email and password is required!');*/
+        return await Api.getInstance()
+        .post('/users/login', { email, password })
     },
 
     async createUser(user) {
-        /*const validProperty = user.validProperty();
-        if (!validProperty) {
-            throw new Error('Tous les champs sont obligatoires!');
-        }
-        const valideEmailType = this.validEmailType(user.email);
-        if (valideEmailType) {*/
-            return await Api.getInstance().post('/users/register', user);
-        /*}
-        throw new Error('Email is not valid');*/
+        return await Api.getInstance().post('/users/register', user);
     },
 
 };
